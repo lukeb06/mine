f95d4838efcbfc805bfe098af82129b7369c00f5
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-11 20:35:41 -0500

    Update

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.camera').forEach(camera => {
     const index = +camera.dataset.index;
-    const port = 3007 - index;
+    const port = 6027 - index;
     camera.src = `http://${window.location.hostname}:${port}`;
 });
