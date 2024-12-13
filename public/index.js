ffd275675e96b83f55dde621f3332563e5d92292
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 02:57:49 -0500

    Update

--- public/index.js
+++ public/index.js
@@ -1,25 +1,29 @@
-document.querySelectorAll('.camera').forEach(camera => {
-    const index = +camera.dataset.index;
-    const port = 6027 - index;
+async function main() {
+    const cameras = Array.from(document.querySelectorAll('.camera'));
+    for await (const camera of cameras) {
+        const index = +camera.dataset.index;
+        const port = 6027 - index;
 
-    const wrapper = document.createElement('div');
-    wrapper.className = 'camera-wrapper';
+        const wrapper = document.createElement('div');
+        wrapper.className = 'camera-wrapper';
 
-    camera.before(wrapper);
-    wrapper.appendChild(camera);
+        camera.before(wrapper);
+        wrapper.appendChild(camera);
 
-    const viewBtn = document.createElement('a');
-    viewBtn.className = 'view-btn';
-    viewBtn.href = `http://${window.location.hostname}:${port}`;
-    viewBtn.textContent = 'Control Cam';
-    wrapper.appendChild(viewBtn);
+        const viewBtn = document.createElement('a');
+        viewBtn.className = 'view-btn';
+        viewBtn.href = `http://${window.location.hostname}:${port}`;
+        viewBtn.textContent = 'Control Cam';
+        wrapper.appendChild(viewBtn);
 
-    camera.src = `http://${window.location.hostname}:${port}`;
+        camera.src = `http://${window.location.hostname}:${port}`;
 
-    try {
-        fetch(camera.src, { mode: 'no-cors' }).then(r => {
+        try {
+            await fetch(camera.src, { mode: 'no-cors' })
             camera.classList.add('active');
             wrapper.classList.add('active');
-        });
-    } catch (e) { }
-});
+        } catch (e) { }
+    }
+}
+
+main();
