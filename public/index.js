00b45c92060b87a44a66fe71037a1b790b229991
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 01:59:15 -0500

    Update

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.camera').forEach(camera => {
     const index = +camera.dataset.index;
     const port = 6027 - index;
-    //camera.src = `http://${window.location.hostname}:${port}`;
+    camera.src = `http://${window.location.hostname}:${port}`;
     try {
         fetch(camera.src, { mode: 'no-cors' }).then(r => {
             const wrapper = document.createElement('div');
