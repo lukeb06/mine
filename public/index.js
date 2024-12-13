05a511d26a84aa9703cdd0b611b620c7f57cb9e4
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 02:04:50 -0500

    Update

--- public/index.js
+++ public/index.js
@@ -1,15 +1,17 @@
 document.querySelectorAll('.camera').forEach(camera => {
     const index = +camera.dataset.index;
     const port = 6027 - index;
+
+    const wrapper = document.createElement('div');
+    wrapper.className = 'camera-wrapper';
+
+    camera.before(wrapper);
+    wrapper.appendChild(camera);
+
     camera.src = `http://${window.location.hostname}:${port}`;
+
     try {
         fetch(camera.src, { mode: 'no-cors' }).then(r => {
-            const wrapper = document.createElement('div');
-            wrapper.className = 'camera-wrapper';
-
-            camera.before(wrapper);
-            wrapper.appendChild(camera);
-
             camera.classList.add('active');
         });
     } catch (e) { }
