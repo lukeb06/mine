efe2f3070114c44e64bfbe590a42bcf670077caa
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 21:18:50 -0500

    Update

--- server.js
+++ server.js
@@ -7,6 +7,36 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
+app.get('/getSkin', async (req, res) => {
+    if (!req.query.name || req.query.name === 'undefined') {
+        sendSteveSkin(res)
+        return
+    }
+
+    const name = req.query.name
+
+    const uuid = await fetch(`https://api.mojang.com/users/profiles/minecraft/${name}?at=${Date.now()}`)
+    if (uuid.status !== 200 || uuid.length === 0) {
+        sendSteveSkin(res)
+        return
+    }
+    const uuidJson = await uuid.json()
+
+    const session = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuidJson.id}`)
+    if (session.status !== 200 || session.length === 0) {
+        sendSteveSkin(res)
+        return
+    }
+    const sessionJson = await session.json()
+
+    const skinData = JSON.parse(Buffer.from(sessionJson.properties[0].value, 'base64').toString('utf8'))
+    const url = skinData.textures.SKIN.url
+
+    const skin = await fetch(url).then(r => r.buffer())
+    res.set('Content-Type', 'image/png').send(skin)
+
+})
+
 app.listen(3008, () => {
     console.log(`http://localhost:3008`);
 });
