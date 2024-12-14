38b2427eba5dbe6d02f94cd85de4fa52022a298f
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-13 21:31:23 -0500

    Update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
