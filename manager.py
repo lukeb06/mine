e3a80e01327412085fce59a148a99dab97420840
Luke Barrier <lukebbarrier06@outlook.com>
2024-12-14 13:16:04 -0500

    Update

--- manager.py
+++ manager.py
@@ -1,18 +1,25 @@
 import subprocess
 from time import sleep
+import os
 
 JOIN_OFFSET = 5
 CAMS = ["cam0.bat", "cam1.bat", "cam2.bat"]
 SERVER = "server.bat"
 
 
-def start(file: str):
-    subprocess.Popen(file)
+def create_background_process(bat_file: str):
+    return subprocess.Popen(f"start {bat_file}", shell=True)
 
 
 if __name__ == "__main__":
-    start(SERVER)
+    create_background_process(SERVER)
 
     for cam in CAMS:
-        start(cam)
+        create_background_process(cam)
         sleep(JOIN_OFFSET)
+
+    while True:
+        input_str = input("> ")
+        if input_str == "q":
+            exit(0)
+            break
